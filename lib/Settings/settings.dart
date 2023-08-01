fd66f9546b8d98d1d70d3eb33d2bdda73a00b0eb
jaivsh <aarshorai22@gmail.com>
2023-08-01 09:11:49 +0530

    added licenses screen

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:vritant/Settings/licenses.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -29,11 +30,16 @@ class _SettingsPageState extends State<SettingsPage> {
                   title: const Text('Share Vritant'),
                   value: const Text('Help to spread a word about the application!'),
                   onPressed: (t) {
-                    Share.share(subject, subject: 'Check out this amazing application!');
-    }
-    ,
+                    Share.share(subject, subject: 'Check out this amazing application!');},
                 ),
-
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.document_scanner_outlined),
+                  title: const Text('Licenses'),
+                  value: const Text('See application\'s licenses'),
+                  onPressed: (t) {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => LicensesScreen()));
+                  },
+                ),
               ],
             ),
           ],
